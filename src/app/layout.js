ddd3df5a1ae144593d8c3faa87702b1e5f72ed5e
AadityaPandey30 <aprock3004@gmail.com>
2025-09-20 15:27:30 +0530

    Updated layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,33 +15,23 @@ const poppins = Poppins({
 export const metadata = {
   title: "YesCity Rewards",
   description: "This page is describes the rewards and campus ambassador program at YesCity",
-  // imageUrl = `${baseUrl}/deep.png`,
-  // return {
-  //   title,
-  //   description,
-  //   metadataBase: new URL(baseUrl),
-  //   openGraph: {
-  //     title,
-  //     description,
-  //     url: currentUrl,
-  //     siteName: 'YesCity',
-  //     type: 'website',
-  //     images: [
-  //       {
-  //         url: imageUrl,
-  //         width: 1200,
-  //         height: 630,
-  //         alt: 'YesCity - Discover Amazing Places in India',
-  //       },
-  //     ],
-  //   },
-  //   twitter: {
-  //     card: 'summary_large_image',
-  //     title,
-  //     description,
-  //     images: [imageUrl],
-  //   },
-  // }
+  keywords: 'Information, City, Travel, YesCity, yescity',
+  author: 'Aaditya Pandey',
+  openGraph: {
+    title: 'YesCity',
+    description:
+      'YesCity helps travellers find right places in each city, exploring their hidden gems and authentic taste.',
+    url: 'https://yescity.in',
+    siteName: 'YesCity',
+    images: [
+        {
+          url: imageUrl,
+          width: 1200,
+          height: 630,
+          alt: 'YesCity - Discover Amazing Places in India',
+        },
+      ],
+  },
   };
 
 export default function RootLayout({ children }) {
